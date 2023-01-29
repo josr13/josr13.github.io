269909b8bef3aa53726cea44fb67541e505c5a17
josr13 <joseph-l-r-r@hotmail.com>
2023-01-29 01:04:15 -0500

    Added mobile navbar + pdf download placeholder

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-const opennav = function() {
-    Document.querySelect(menu-svg)
-}
+const menu = document.querySelector('.menu-svg');
+const close = document.querySelector('.close-svg');
+const nav = document.querySelector('nav');
+
+menu.addEventListener('click', () => {
+    nav.classList.add('nav-opened');
+});
+
+close.addEventListener('click', () => {
+    nav.classList.remove('nav-opened');
+});
